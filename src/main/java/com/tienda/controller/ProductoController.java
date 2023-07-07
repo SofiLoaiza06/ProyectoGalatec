da26c54d20adc61f8a018359cba190a620fd7dff
JoseUrenaO <jauo2810@gmail.com>
2023-07-07 10:49:02 -0600

    Semana 8

--- src/main/java/com/tienda/controller/ProductoController.java
+++ src/main/java/com/tienda/controller/ProductoController.java
@@ -1,6 +1,7 @@
 package com.tienda.controller;
 
 import com.tienda.domain.Producto;
+import com.tienda.service.CategoriaService;
 import com.tienda.service.ProductoService;
 import com.tienda.service.FirebaseStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,8 +20,14 @@ public class ProductoController {
     @Autowired
     private ProductoService productoService;
     
+    @Autowired
+    private CategoriaService categoriaService;
+    
     @GetMapping("/listado")
     public String listado(Model model) {
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias",categorias);
+        
         var productos = productoService.getProductos(false);
         model.addAttribute("productos",productos);
         model.addAttribute("totalProductos",productos.size());
@@ -58,6 +65,9 @@ public class ProductoController {
 
     @GetMapping("/modificar/{idProducto}")
     public String productoModificar(Producto producto, Model model) {
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias",categorias);
+        
         producto = productoService.getProducto(producto);
         model.addAttribute("producto", producto);
         return "/producto/modifica";
